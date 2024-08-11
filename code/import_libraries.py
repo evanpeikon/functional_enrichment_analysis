1acebf7d8389f98c9ba35c27b7ce2e53d4ed2a4f
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2024-08-11 10:24:27 -0400

    Create import_libraries.py

---
+++ code/import_libraries.py
@@ -0,0 +1,10 @@
+# pip install packages
+!pip install gprofiler
+!pip install gseapy
+
+# import libraries
+import pandas as pd
+from gprofiler import GProfiler
+import matplotlib.pyplot as plt
+import seaborn as sns
+import gseapy as gp
